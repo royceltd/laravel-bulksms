a58a2ad47db0375b118eafccf0aa1991adfe8f24
Royce Technologies Ltd <developer@roycetechnologies.co.ke>
2020-12-05 19:08:47 +0300

    updated tags

--- src/LaravelBulkSMSServiceProvider.php
+++ src/LaravelBulkSMSServiceProvider.php
@@ -37,7 +37,7 @@ class LaravelBulkSMSServiceProvider extends ServiceProvider
                 $this->publishes([
                     __DIR__ . '/../database/migrations/create_sent_text_messages_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_sent_text_messages_table.php'),
                     // you can add any number of migrations here
-                ], 'migrations');
+                ], 'roycebulksms');
             }
         }
     }
