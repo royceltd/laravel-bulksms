56f1637406f03af1ffe404800f1fd497e6e5f765
Josphat Kipyegon <josphatkips@gmail.com>
2023-04-16 10:21:19 +0300

    updates

--- database/migrations/2021_11_26_073425_sent_texd_messages.php
+++ database/migrations/2021_11_26_073425_sent_texd_messages.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-class CreateSendTextMessagesTable extends Migration
+class CreateSendTable extends Migration
 {
     /**
      * Run the migrations.
